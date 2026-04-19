0ea8df7f5a0b6f9ba8c9ab52c0da5aaff1733b4a
022433 <hojin022433@gmail.com>
2026-04-19 19:42:41 +0900

    UI 제출

---
+++ animal/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='all'>
+    <div className="background">
+  
+  </div>
+    
+    <div className="main-container">
+
+      <div className="content-box">
+        <div className="sprout-icon">🌱</div>
+        <h2 className="title">C뿌리기</h2>
+        <button className="start-button">
+          공부 시작하기
+        </button>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
